37cc78e71c0d66ba641adcc73dc96a60785474af
Francisco Oliveira <fr.pdoliv@gmail.com>
2026-08-29 10:15:57 +0100

    feat: creates the data model for the UI framework

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Does Nothing");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UI;
+using UI.HomeScreen;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddTransient<Application>();
+
+using var host = builder.Build();
+
+var app = host.Services.GetRequiredService<Application>();
+await app.RunAsync(new HomeScreen());
